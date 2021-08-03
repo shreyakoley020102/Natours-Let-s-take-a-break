dfbbadec59d551b89d7314d3ad2692eb6f3566b2
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-03 10:54:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.status(200).send('Hello!!');
+    res.status(200)
+    .json({message: 'Hello!!', app:'Natours'});
 });
 
 const port = 3000;
