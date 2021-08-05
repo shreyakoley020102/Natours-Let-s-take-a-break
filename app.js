0dc350f1f7d381295dfad754535fa5a06467b42f
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-05 20:10:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const tours = JSON.parse(
 app.get('/api/v1/tours',(req,res)=>{
     res.status(200).json({
         status: 'success',
+        results: tours.length,
         data:{
             tours
         }
