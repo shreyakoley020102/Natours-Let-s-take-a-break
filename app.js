c19c51d33b2eca181f92f811e93250cca848e57a
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-05 20:08:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+const fs=require('fs')
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    res
-    .status(200)
-    .json({message: 'Hello!!', app:'Natours'});
-});
+// app.get('/', (req,res)=>{
+//     res
+//     .status(200)
+//     .json({message: 'Hello!!', app:'Natours'});
+// });
 
-app.post('/',(req,res)=>{
-    res.send('You can post to this..')
+// app.post('/',(req,res)=>{
+//     res.send('You can post to this..')
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        data:{
+            tours
+        }
+    })
 })
 
 const port = 3000;
