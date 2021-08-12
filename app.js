3f5c09734e14caf22f11f7feca7caf8ee7a87a83
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-12 17:05:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const fs=require('fs')
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+
+app.use(morgan('dev'))
 app.use(express.json())
 
 app.use((req,res, next)=>{
@@ -17,6 +22,8 @@ app.use((req,res, next)=>{
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req,res)=>{
     console.log(req.requestTime);
@@ -109,6 +116,8 @@ const deleteTour = (req,res)=>{
 // app.patch('/api/v1/tours/:id',updateTour)
 // app.delete('/api/v1/tours/:id',deleteTour)
 
+// 3) ROUTES
+
 app
    .route('/api/v1/tours')
    .get(getAllTours)
@@ -121,6 +130,8 @@ app
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour)
+
+// 4) ATART THE SERVER
 
 const port = 3000;
 app.listen(port,()=>{
