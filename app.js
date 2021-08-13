5af516929dadfc1d6e64c5c8f164a93a30c6312e
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-13 09:32:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ const getAllUsers = (req,res) =>{
         message: 'This route is not defined!'
     })
 }
-const getAllUsers = (req,res) =>{
+const getUsers = (req,res) =>{
     res.status(500).json({
         status: 'error',
         message: 'This route is not defined!'
