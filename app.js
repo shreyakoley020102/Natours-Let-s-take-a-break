5b5969955899d55a62519dd4e9d6f15298d4017b
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-13 09:34:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -143,12 +143,6 @@ const deleteUser = (req,res) =>{
     })
 }
 
-// app.get('/api/v1/tours',getAllTours)
-// app.post('/api/v1/tours', createTour)
-// app.get('/api/v1/tours/:id',getTour)
-// app.patch('/api/v1/tours/:id',updateTour)
-// app.delete('/api/v1/tours/:id',deleteTour)
-
 // 3) ROUTES
 
 app
