e9338ebf6706c40cba545f4f10183db49bd6bcc2
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-15 10:53:41 +0530

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const getAllUsers = (req,res) =>{
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined!'
+    })
+}
+const getUser = (req,res) =>{
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined!'
+    })
+}
+const createUser = (req,res) =>{
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined!'
+    })
+}
+const updateUser = (req,res) =>{
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined!'
+    })
+}
+const deleteUser = (req,res) =>{
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not defined!'
+    })
+}
+
+const router = express.Router()
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser)
+
+router
+    .route('/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser)
+
+module.exports = router;
