65c31ecf4f2785d7f7ea2c4658c9c911d3729080
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-16 22:35:04 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(morgan('dev'))
 app.use(express.json())
+//app.use(express.static(`${__dirname}/public`))
+app.use(express.static('./public'));
 
 app.use((req,res, next)=>{
     console.log('Hello from middleware!');
